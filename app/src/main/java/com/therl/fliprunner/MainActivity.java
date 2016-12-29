56f59c97ca98e08e129834965b69fc58c52f7682
rachel <rachellee1098@gmail.com>
2016-12-29 03:43:52 -0500

    building an intent

--- app/src/main/java/com/therl/fliprunner/MainActivity.java
+++ app/src/main/java/com/therl/fliprunner/MainActivity.java
@@ -1,7 +1,10 @@
 package com.therl.fliprunner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+
     }
 }
